054294d31e9b24dc2c630777b70a4013668be77a
rortms <rortms@gmail.com>
2019-02-20 13:56:56 -0800

    Added function that takes a word sequence and generates skip-gram labels for each word in the sequence. That is, for a given word a sample word is taken from a fixed window around it, which becomes a label for the word. A repeat parameter will repeat this process k times for each word, i.e., using the word k times as input with k potentialy different labels.

--- toy_w2v.py
+++ toy_w2v.py
@@ -1,18 +1,19 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[72]:
 
 
 import numpy as np
 import pandas as pd
+import itertools as itt
 
 
-# In[7]:
+# In[2]:
 
 
 # Infinite data iterator
-def iterData(filename, batch_size):
+def iterData(filename, batch_size, jump_around=False):
 
     # Iterator reading file in batches
     def get_batch():
@@ -22,22 +23,27 @@ def iterData(filename, batch_size):
             position, offset = 0, batch_size
 
             while True:
-                 yield datfile.read(batch_size)
-                 position = datfile.seek(position+batch_size)
+                
+                if jump_around:
+                    offset = np.random.randint(1,4) * batch_size
+                    
+                yield datfile.read(batch_size)
+                position = datfile.seek(position + offset)
 
     batch = get_batch()
 
-    # When file is exhausted, start over with new get_batch ierator
+    # When file is exhausted, start over with new get_batch iterator
     while True:
         b = next(batch)
         if b:
-            yield  b
+            yield  b.split()
         else:
+            print("EOF, starting over")
             batch = get_batch()
         
 
 
-# In[8]:
+# In[3]:
 
 
 # Test infinite iterator
@@ -49,14 +55,75 @@ print(len(lines))
 
 # Only 54 charactors reused indefinitely
 testIter = iterData(fn, 5)
-print(list( (next(testIter) for k in range(100)) ) )
+print(list( (" ".join(next(testIter)) for k in range(100)) ) )
 
 
-# In[ ]:
+# In[4]:
 
 
-with open("./config", "r") as conf:
-    data_file = conf.read().split(1)
+## Initialize file iterator
+from config import settings
 
-moreData = iterData(data_file, 1000)
+moreData = iterData(settings["data_path"], 1000)
+
+
+# In[5]:
+
+
+# Construct vocabulary set
+from collections import Counter
+
+words = Counter(next(moreData))
+print(len(words))
+
+for k in range(70000):
+    words.update(next(moreData))
+    if k % 3000 == 0:
+        print(len(words))
+
+
+# In[6]:
+
+
+# Remove less commmon words
+print(len(words))
+words = Counter({ w : count  for w, count in words.most_common() if count > 5 })
+print(len(words))
+
+
+# In[7]:
+
+
+# Word to id mappings
+word2int = { tup[0] : i for i, tup in enumerate(words.most_common()) }
+int2word = { i : word for word, i in word2int.items() }
+
+
+# In[89]:
+
+
+# Skip-gram model: for each word, sample a surrounding word within a fixed window (excluding itself),
+# (Forgot actual terminology) each word is processed in this way k times generating k training targets for it.
+#
+
+def inputsTargets(word_sequence, radius = 4, repeat_num = 2):
+
+    words, targets = [None]*len(word_sequence)*repeat_num, [None]*(len)(word_sequence)*repeat_num
+    batch_size = len(words) # or targets
+    
+    for i in range(0,batch_size, repeat_num):
+        
+        # Index in actual sequence
+        index = i // repeat_num
+        word = word_sequence[index]
+        
+        lower = 0 if index < radius else index - radius
+        upper = index +radius
+        words_in_window = word_sequence[lower:index] + word_sequence[index+1:upper]
+        
+        for k in range(repeat_num):
+            words[i+k] = word
+            targets[i+k] = words_in_window[np.random.randint(0,len(words_in_window))]
+
+    return words, targets
 
