2a90124583f047a579d63dc2205038a9087f2c98
rortms <rortms@gmail.com>
2019-02-23 12:20:20 -0800

    Added subsampling imitating the original word 2 vec see breakdown of methods; http://mccormickml.com/2017/01/11/word2vec-tutorial-part-2-negative-sampling/

--- toy_w2v.py
+++ toy_w2v.py
@@ -1,11 +1,12 @@
 
 # coding: utf-8
 
-# In[72]:
+# In[1]:
 
 
 import numpy as np
 import pandas as pd
+from collections import Counter
 import itertools as itt
 
 
@@ -71,23 +72,34 @@ moreData = iterData(settings["data_path"], 1000)
 
 
 # Construct vocabulary set
-from collections import Counter
-
 words = Counter(next(moreData))
 print(len(words))
-
 for k in range(70000):
     words.update(next(moreData))
     if k % 3000 == 0:
         print(len(words))
+        
 
 
 # In[6]:
 
 
-# Remove less commmon words
+# Subsampling with word2vec's subsampling function
+# following: http://mccormickml.com/2017/01/11/word2vec-tutorial-part-2-negative-sampling/
+
+# Increase the chance of a less common word being kept, and reduce that of very common words
+def P(word_fraction):
+
+    return np.sqrt(1e-7/word_fraction) + 1e-7/word_fraction
+
 print(len(words))
-words = Counter({ w : count  for w, count in words.most_common() if count > 5 })
+
+m = min(words.values())
+
+total_words = sum(words.values())
+
+words = Counter({ w : int( (count/m)**0.75 )  for w, count in words.most_common() if np.random.uniform() >= P(count/total_words)})
+
 print(len(words))
 
 
@@ -99,7 +111,7 @@ word2int = { tup[0] : i for i, tup in enumerate(words.most_common()) }
 int2word = { i : word for word, i in word2int.items() }
 
 
-# In[89]:
+# In[8]:
 
 
 # Skip-gram model: for each word, sample a surrounding word within a fixed window (excluding itself),
@@ -113,12 +125,12 @@ def inputsTargets(word_sequence, radius = 4, repeat_num = 2):
     
     for i in range(0,batch_size, repeat_num):
         
-        # Index in actual sequence
+        # Index in word_sequence array ( len(word_sequence) < batch_size )
         index = i // repeat_num
         word = word_sequence[index]
         
         lower = 0 if index < radius else index - radius
-        upper = index +radius
+        upper = index + radius
         words_in_window = word_sequence[lower:index] + word_sequence[index+1:upper]
         
         for k in range(repeat_num):
@@ -126,4 +138,32 @@ def inputsTargets(word_sequence, radius = 4, repeat_num = 2):
             targets[i+k] = words_in_window[np.random.randint(0,len(words_in_window))]
 
     return words, targets
+
+
+# In[ ]:
+
+
+class littleNN(object):
+
+    def __init__(self, word_counts, embedding_dim, neg_sample_size):
+
+        #Network parameters
+        self.input2hidden = np.random.uniform(
+
+    
+
+
+# In[30]:
+
+
+testN = littleNN(words, 300, 5)
+print(testN.forward_pass(word2int['weather']))
+testN.negSample()
+
+
+# In[9]:
+
+
+t = np.array([1,2,3,4,5,74,7,78])
+t[[0,5,3]]
 
